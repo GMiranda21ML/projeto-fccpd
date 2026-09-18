e6225de7de1d75721390d0f2b7d77b08c175dba6
GMiranda21ML <gabriel.mucarbel.lima@gmail.com>
2026-09-18 19:44:50 -0300

    enviando pasta do projeto

---
+++ src/main/java/br/com/cem/fccpd/parteum/ParteumApplication.java
@@ -0,0 +1,13 @@
+package br.com.cem.fccpd.parteum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParteumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParteumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cem/fccpd/parteum/ParteumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cem.fccpd.parteum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParteumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
